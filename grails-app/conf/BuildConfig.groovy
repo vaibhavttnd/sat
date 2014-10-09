584cbaefcd2a1bc06e80abad4a536863c02198eb
Prakriti Sharma <prakritis@intelligrape.com>
2014-10-09 10:43:37 +0530

    Removed plugins

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -61,7 +61,7 @@ grails.project.dependency.resolution = {
 
         // plugins for the compile step
         compile ":scaffolding:2.1.2"
-        compile ':cache:1.1.7'
+       // compile ':cache:1.1.7'
         compile ":asset-pipeline:1.9.6"
         compile ":twitter4j:4.0.4.0"
         compile ':spring-security-oauth:2.1.0-SNAPSHOT'
@@ -71,7 +71,7 @@ grails.project.dependency.resolution = {
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.5.5" // or ":hibernate:3.6.10.17"
-        runtime ":database-migration:1.4.0"
+      //  runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
 
         // Uncomment these to enable additional asset-pipeline capabilities
