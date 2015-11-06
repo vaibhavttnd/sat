12b091947d1952e464edf345e67057efb50601de
Neha_Gupta <neha.gupta@intelligrape.com>
2015-11-06 17:39:29 +0530

    Refactored the code to remove java.util.ConcurrentModificationException;

--- grails-app/services/com/tweetAmp/CategoryService.groovy
+++ grails-app/services/com/tweetAmp/CategoryService.groovy
@@ -8,17 +8,15 @@ class CategoryService {
     def updateUsersForCategories(Category category, List<User> userList) {
         if (userList*.id != category.users*.id) {
             Set<User> categoryUsers = category.users ?:[]
-            categoryUsers?.each { User user ->
-                if (!userList?.contains(user)) {
-                    user.removeFromCategories(category)
-                }
-            }
-            category.save(flush: true, failOnError: true)
 
-            userList.each { User user ->
-                if (!categoryUsers?.contains(user)) {
-                    user.addToCategories(category)
-                }
+            // Deleting the mentioned users
+            (categoryUsers-userList)?.each {User user ->
+                user.removeFromCategories(category)
+            }
+
+            //Adding the other mentioned users
+            (userList-categoryUsers)?.each { User user ->
+                user.addToCategories(category)
             }
             category.save(flush: true, failOnError: true)
         }
