478312c517e1949d0933b880efe76764cdc6c74a
Neha_Gupta <neha.gupta@intelligrape.com>
2015-11-27 17:05:29 +0530

    Added hql to fetch the retweet record with least timestamp and status as pending.

--- grails-app/jobs/com/tweetAmp/RetweetJob.groovy
+++ grails-app/jobs/com/tweetAmp/RetweetJob.groovy
@@ -5,30 +5,24 @@ import twitter4j.Twitter
 
 class RetweetJob {
 
-    def twitterService
-    static triggers = {
-        simple repeatInterval: randomTimeGenerator() * 60 * 1000 //300000l // execute job once in 10-20 minutes
-    }
+	def twitterService
+	static triggers = {
+		simple repeatInterval: 3 * 60 * 1000 //300000l // execute job once in 3 minutes
+	}
 
-    def execute() {
-        log.info("RetweetJob Triggered -----------------------------------@ ${new Date()}")
-        Twitter twitter = twitterService.twitter
-        def tweetsRetweeted1= TweetsRetweeted.createCriteria().get {
-            projections {
-                 min "reTweetTime"
+	def execute() {
+		log.info("RetweetJob Triggered -----------------------------------@ ${new Date()}")
+		Twitter twitter = twitterService.twitter
+
+		def results = TweetsRetweeted.executeQuery("select tweets from TweetsRetweeted as tweets where status=:status order by reTweetTime asc", [status: RetweetStatus.PENDING, max: 1])
+		println ">>>>>>>>>>>>>>>....min time>>>>>>>>>>>>>>>>>>>>>>>  " + results
+            TweetsRetweeted tweetsRetweeted = results.first()
+            if (tweetsRetweeted && tweetsRetweeted.reTweetTime<=(new Date()).getTime()) {
+                log.info("**********************RETWEETING*************************** tweet id " + tweetsRetweeted?.id)
+                TwitterUser twitterUser = tweetsRetweeted?.twitterUser
+                User user = User.findByTwitterUser(twitterUser)
+                twitterService.retweetWithSpecificUser(user, twitter, tweetsRetweeted?.reTweetId)
             }
-        } as Long
-        println ">>>>>>>>>>>>>>>....min time>>>>>>>>>>>>>>>>>>>>>>>  "+tweetsRetweeted1
-        TweetsRetweeted tweetsRetweeted = TweetsRetweeted.findByStatus(RetweetStatus.PENDING)
-        if (tweetsRetweeted) {
-            log.info("**********************RETWEETING*************************** tweet id " + tweetsRetweeted?.id)
-            TwitterUser twitterUser = tweetsRetweeted?.twitterUser
-            User user = User.findByTwitterUser(twitterUser)
-            twitterService.retweetWithSpecificUser(user, twitter, tweetsRetweeted?.reTweetId)
-        }
-    }
+	}
 
-    static int randomTimeGenerator(){
-        new Random().nextInt(11) + 10
-    }
 }
