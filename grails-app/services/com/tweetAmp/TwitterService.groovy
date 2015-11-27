ec70ae4968ee19ae606d36b9b684887468ed1520
Neha_Gupta <neha.gupta@intelligrape.com>
2015-11-27 11:11:43 +0530

    updated the grails console. And handling the revoked access retweets.

--- grails-app/services/com/tweetAmp/TwitterService.groovy
+++ grails-app/services/com/tweetAmp/TwitterService.groovy
@@ -9,93 +9,98 @@ import twitter4j.auth.AccessToken
 @Transactional
 class TwitterService {
 
-    def grailsApplication
+	def grailsApplication
 
-    Twitter getTwitter() {
-        def twitterConfig = grailsApplication.config.twitter4j
-        String consumerKey = twitterConfig.'default'.OAuthConsumerKey ?: ''
-        String consumerSecret = twitterConfig.'default'.OAuthConsumerSecret ?: ''
+	Twitter getTwitter() {
+		def twitterConfig = grailsApplication.config.twitter4j
+		String consumerKey = twitterConfig.'default'.OAuthConsumerKey ?: ''
+		String consumerSecret = twitterConfig.'default'.OAuthConsumerSecret ?: ''
 
-        Twitter twitter = new TwitterFactory().getInstance();
-        twitter.setOAuthConsumer(consumerKey, consumerSecret)
-        return twitter
-    }
+		Twitter twitter = new TwitterFactory().getInstance();
+		twitter.setOAuthConsumer(consumerKey, consumerSecret)
+		return twitter
+	}
 
-    List<TwitterCredentialDTO> getTwitterCredentials() {
-        TwitterUser.createCriteria().list {
-            projections {
-                groupProperty('screenName')
-                property('id')
-                property('accessToken')
-                property('accessTokenSecret')
-            }
-        }.collect {
-            new TwitterCredentialDTO(id: it[1], screenName: it[0], accessToken: it[2], accessTokenSecret: it[3])
-        }
+	List<TwitterCredentialDTO> getTwitterCredentials() {
+		TwitterUser.createCriteria().list {
+			projections {
+				groupProperty('screenName')
+				property('id')
+				property('accessToken')
+				property('accessTokenSecret')
+			}
+		}.collect {
+			new TwitterCredentialDTO(id: it[1], screenName: it[0], accessToken: it[2], accessTokenSecret: it[3])
+		}
 
-    }
+	}
 
-    TweetsRetweeted retweet(TwitterCredentialDTO dto, Twitter twitter, Long id) {
-        AccessToken accessToken = new AccessToken(dto.accessToken, dto.accessTokenSecret)
-        twitter.setOAuthAccessToken(accessToken)
-        TweetsRetweeted tweetsRetweeted = TweetsRetweeted.findByTwitterUserAndReTweetId(TwitterUser.load(dto.id), id)
-        try {
-            if (!tweetsRetweeted) {
-                twitter.retweetStatus(id)
-                tweetsRetweeted = new TweetsRetweeted(reTweetId: id, twitterUser: TwitterUser.load(dto.id)).save(flush: true);
-            }
-        }
-        catch (Exception e) {
-            println "Error in retweeting status ${e.message}"
-        }
-        return tweetsRetweeted
-    }
+	TweetsRetweeted retweet(TwitterCredentialDTO dto, Twitter twitter, Long id) {
+		AccessToken accessToken = new AccessToken(dto.accessToken, dto.accessTokenSecret)
+		twitter.setOAuthAccessToken(accessToken)
+		TweetsRetweeted tweetsRetweeted = TweetsRetweeted.findByTwitterUserAndReTweetId(TwitterUser.load(dto.id), id)
+		try {
+			if (!tweetsRetweeted) {
+				twitter.retweetStatus(id)
+				tweetsRetweeted = new TweetsRetweeted(reTweetId: id, twitterUser: TwitterUser.load(dto.id)).save(flush: true);
+			}
+		}
+		catch (Exception e) {
+			println "Error in retweeting status ${e.message}"
+		}
+		return tweetsRetweeted
+	}
 
-    TweetsRetweeted retweetWithSpecificUser(User user, Twitter twitter, Long tweetId) {
-        println "retweeting*************************************************" + user?.email
-        AccessToken accessToken = new AccessToken(user.twitterUser.accessToken, user.twitterUser.accessTokenSecret)
-        twitter.setOAuthAccessToken(accessToken)
-        TweetsRetweeted tweetsRetweeted = TweetsRetweeted.findByTwitterUserAndReTweetId(user.twitterUser, tweetId)
-        if (tweetsRetweeted) {
-            println "found******************************"
-            try {
-                if (tweetsRetweeted.status == RetweetStatus.PENDING) {
-                    println "tweet id        " + tweetId
-                    twitter.retweetStatus(tweetId)
-                    tweetsRetweeted.status = RetweetStatus.DONE
-                    tweetsRetweeted.save(flush: true);
-                }
-            }
-            catch (TwitterException e) {
-                tweetsRetweeted.status = RetweetStatus.REJECTED
-                tweetsRetweeted.save(flush: true);
-                log.info("Error in retweeting status: \n${e.message}\n" + e.errorMessage)
-                log.info("Status Code*************************************************\n" + e.getStatusCode())
-            }
-        } else {
-            println "not found******************************************"
-        }
-        return tweetsRetweeted
-    }
+	TweetsRetweeted retweetWithSpecificUser(User user, Twitter twitter, Long tweetId) {
+		println "retweeting*************************************************" + user?.email
+		AccessToken accessToken = new AccessToken(user.twitterUser.accessToken, user.twitterUser.accessTokenSecret)
+		twitter.setOAuthAccessToken(accessToken)
+		TweetsRetweeted tweetsRetweeted = TweetsRetweeted.findByTwitterUserAndReTweetId(user.twitterUser, tweetId)
+		if (tweetsRetweeted) {
+			println "found******************************"
+			if (user?.twitterUser?.accessToken) {
+				try {
+					if (tweetsRetweeted.status == RetweetStatus.PENDING) {
+						println "tweet id        " + tweetId
+						twitter.retweetStatus(tweetId)
+						tweetsRetweeted.status = RetweetStatus.DONE
+						tweetsRetweeted.save(flush: true);
+					}
+				}
+				catch (TwitterException e) {
+					tweetsRetweeted.status = RetweetStatus.REJECTED
+					tweetsRetweeted.save(flush: true);
+					log.info("Error in retweeting status: \n${e.message}\n" + e.errorMessage)
+					log.info("Status Code*************************************************\n" + e.getStatusCode())
+				}
+			} else {
+				tweetsRetweeted.status= RetweetStatus.REJECTED
+				tweetsRetweeted.save(flush: true);
+			}
+		} else {
+			println "not found******************************************"
+		}
+		return tweetsRetweeted
+	}
 
-    void createNewObjects(Set<Long> userIds, Long tweetId) {
-        try {
-            Set<User> users = User.getAll(userIds.toList())
-            users.each { User user ->
-                TwitterUser credential = user.twitterUser
-                TweetsRetweeted tweetsRetweeted = TweetsRetweeted.findByTwitterUserAndReTweetId(user.twitterUser, tweetId)
-                if (credential && !tweetsRetweeted) {
-                    tweetsRetweeted = new TweetsRetweeted(reTweetId: tweetId, status: RetweetStatus.PENDING)
-                    tweetsRetweeted.reTweetId = tweetId
-                    tweetsRetweeted.twitterUser = credential
-                    credential.addToRetweets(tweetsRetweeted).save(failOnError: true, flush: true)
-                }
-            }
-            println "*********************************Objects created*********************************************"
-        }
-        catch (Exception e) {
-            println ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n" + e
-            e.printStackTrace(System.out)
-        }
-    }
+	void createNewObjects(Set<Long> userIds, Long tweetId) {
+		try {
+			Set<User> users = User.getAll(userIds.toList())
+			users.each { User user ->
+				TwitterUser credential = user.twitterUser
+				TweetsRetweeted tweetsRetweeted = TweetsRetweeted.findByTwitterUserAndReTweetId(user.twitterUser, tweetId)
+				if (credential && !tweetsRetweeted) {
+					tweetsRetweeted = new TweetsRetweeted(reTweetId: tweetId, status: RetweetStatus.PENDING)
+					tweetsRetweeted.reTweetId = tweetId
+					tweetsRetweeted.twitterUser = credential
+					credential.addToRetweets(tweetsRetweeted).save(failOnError: true, flush: true)
+				}
+			}
+			println "*********************************Objects created*********************************************"
+		}
+		catch (Exception e) {
+			println ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n" + e
+			e.printStackTrace(System.out)
+		}
+	}
 }
